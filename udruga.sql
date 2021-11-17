3bf0f5cef5b7b3000de546c2bd2952b8ca9209c8
JankovicFilip <93282802+JankovicFilip@users.noreply.github.com>
2021-11-17 22:47:44 +0100

    Novi update za udrugu

--- udruga.sql
+++ udruga.sql
@@ -8,23 +8,19 @@ create table udruga(
     sifra int not null primary key auto_increment,
     naziv varchar(50) not null,
     djelatnici int,
-    sticenici int,
     prostor int,
     volonteri int
     );
 
 create table djelatnici(
     sifra int not null primary key auto_increment,
-    ime varchar(50) not null,
-    prezime varchar(50) not null,
-    sticenici int,
-    udruga varchar(50) not null
+    osoba int not null,
+    sticenici int not null
 );
 
 create table sticenici(
     sifra int not null primary key auto_increment,
-    naziv varchar(50) not null,
-    djelatnici int
+    naziv varchar(50) not null
 );
 
 create table prostor(
@@ -34,19 +30,72 @@ create table prostor(
 
 create table volonteri(
     sifra int not null primary key auto_increment,
-    ime varchar(50) not null,
-    prezime varchar(50) not null,
-    udruga varchar(50) not null
+    osoba int not null,
+    sticenici int not null
 );
 
 create table osoba(
-    volonteri int,
-    djelatnici int
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50)
 );
 
 alter table udruga add foreign key (prostor) references prostor(sifra);
 alter table udruga add foreign key (djelatnici) references djelatnici(sifra);
 alter table udruga add foreign key (volonteri) references volonteri(sifra);
-alter table sticenici add foreign key (djelatnici) references djelatnici(sifra);
-alter table osoba add foreign key (volonteri) references volonteri(sifra);
-alter table osoba add foreign key (djelatnici) references djelatnici(sifra);
+
+alter table djelatnici add foreign key (osoba) references osoba(sifra);
+alter table djelatnici add foreign key (sticenici) references sticenici(sifra);
+alter table volonteri add foreign key (osoba) references osoba(sifra);
+alter table volonteri add foreign key (sticenici) references sticenici(sifra);
+
+
+
+# 1-4 12, 20, 32, 120
+
+insert into prostor (sifra,kvadratura) values
+(null,12),
+(null,20),
+(null,32),
+(null,120);
+
+
+# 1-4
+
+insert into osoba (sifra,ime,prezime) values
+(null,'Marija','Marić'),
+(null,'Petra','Petrić'),
+(null,'Marin','Marinković'),
+(null,'Ivan','Ivanušec');
+
+
+
+
+# 1-4
+
+insert into sticenici (sifra,naziv) values
+(null,'Njemački ovčar'),
+(null,'Husky'),
+(null,'Aljaski malamut'),
+(null,'Siberijska mačka');
+
+
+# 1-2
+
+insert into djelatnici (sifra,osoba,sticenici) values
+(null,1,1),
+(null,2,2);
+
+
+
+# 1-2
+
+insert into volonteri (sifra,osoba,sticenici) values
+(null,3,3),
+(null,4,4);
+
+# 1-2
+
+insert into udruga (sifra,naziv,djelatnici,prostor,volonteri) values
+(null,'Azil Osijek',1,4,1),
+(null,'Azil Osijek',2,3,2);
